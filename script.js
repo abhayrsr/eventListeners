ca10fed37bb1470183f330e21a277dc9d46aeee1
abhay <abhayrajsinghrana123@gmail.com>
2023-06-05 18:02:37 +0530

    feat: mouse event

--- script.js
+++ script.js
@@ -18,3 +18,29 @@ stopButton.addEventListener("click", () => {
     //stopButton.classList.add("btn_red");
     stopButton.classList.toggle("btn_red");
 })
+
+let input = document.querySelector(".input_box");
+
+// input.addEventListener("change", () => {
+//     console.log(input.value);
+// })
+
+// input.addEventListener("focus", () => {
+//     console.log(input.value);
+// })
+
+// input.addEventListener("input", () => {
+//     console.log(input.value);
+// })
+
+// input.addEventListener("keyup", () => {
+//     console.log(input.value);
+// })
+
+input.addEventListener("mousedown",(event) => {
+    console.log(event.button);
+    let xCor = event.pageX;
+    let yCor = event.pageY;
+    let cor = `xCor - ${xCor}, yCor - ${yCor}`;
+    console.log(cor); 
+})
